838152b743b335213d329aeeeaa57f59369fcba2
bala-amypo <bala@amypo.in>
2025-12-21 12:18:46 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +13,12 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Resource Allocation Optimizer API")
+                        .description("API for managing shared resources, requests, and allocations")
+                        .version("1.0.0"))
                 .servers(List.of(
                         new Server().url("https://9005.vs.amypo.ai")
                 ));
-        }
+    }
 }
